87145cf33535d38ff6ff533685e8aa0b4b67fca1
Leonardo Oliveira <leonardo.sousaoliver@gmail.com>
2026-06-03 19:19:04 -0300

    fix: Arruma o requestbody de criacao da transacao

--- src/main/java/br/com/desafioitau/controllers/TransacaoController.java
+++ src/main/java/br/com/desafioitau/controllers/TransacaoController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +18,7 @@ public class TransacaoController {
     private final TransacaoRepository repository;
 
     @PostMapping
-    public ResponseEntity<Void> cadastrar(Transacao transacao){
+    public ResponseEntity<Void> cadastrar(@RequestBody Transacao transacao){
         transacao = repository.save(transacao);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(null);
