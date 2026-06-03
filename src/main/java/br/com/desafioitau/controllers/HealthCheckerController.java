40e424c458a7e1ca241e12c247b3c0e53a71730f
Leonardo Oliveira <leonardo.sousaoliver@gmail.com>
2026-06-03 20:27:39 -0300

    feat: Cria HealthChecker

--- src/main/java/br/com/desafioitau/controllers/HealthCheckerController.java
+++ src/main/java/br/com/desafioitau/controllers/HealthCheckerController.java
@@ -1,5 +1,7 @@
 package br.com.desafioitau.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +11,11 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/health")
+@Tag(name = "Health Checker")
 public class HealthCheckerController {
 
     @GetMapping
+    @Operation(summary = "Confere se a API esta UP")
     public ResponseEntity<Map<String, String>> health(){
         return ResponseEntity.ok(Map.of("status", "UP"));
     }
