c01d66c1e1bf20e1f1fe606b6f39c9651d4821c9
Leonardo Oliveira <leonardo.sousaoliver@gmail.com>
2026-06-03 20:03:32 -0300

    feat: Cria Loggers para Controllers

--- src/main/java/br/com/desafioitau/controllers/EstatisticaController.java
+++ src/main/java/br/com/desafioitau/controllers/EstatisticaController.java
@@ -3,6 +3,8 @@ package br.com.desafioitau.controllers;
 import br.com.desafioitau.entities.Estatistica;
 import br.com.desafioitau.service.EstatisticaService;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,10 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class EstatisticaController {
 
+    private final Logger logger = LoggerFactory.getLogger(EstatisticaController.class);
+
     private final EstatisticaService service;
 
     @GetMapping
     public ResponseEntity<Estatistica> stats(){
+        logger.info("Pegando as ultimas transações dos ultimos 60 segundos");
+
         return ResponseEntity.ok(service.obterEstatisticas());
     }
 }
--- src/main/java/br/com/desafioitau/controllers/TransacaoController.java
+++ src/main/java/br/com/desafioitau/controllers/TransacaoController.java
@@ -4,6 +4,8 @@ import br.com.desafioitau.entities.Transacao;
 import br.com.desafioitau.repositories.TransacaoRepository;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,12 +15,15 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class TransacaoController {
 
+    private final Logger logger = LoggerFactory.getLogger(TransacaoController.class);
     private final TransacaoRepository repository;
 
     @PostMapping
     public ResponseEntity<Void> cadastrar(@RequestBody @Valid Transacao transacao){
+        logger.info("Transação iniciada");
         transacao = repository.save(transacao);
 
+        logger.info("Transação finalizada com sucesso {}", transacao);
         return ResponseEntity.status(HttpStatus.CREATED).body(null);
     }
 
